c521ffb0691fb294b98da3ebcb3697061f46e45c
Matthew Chan <mattkc7@gmail.com>
2013-08-21 20:50:26 -0700

    Initial set up

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FancyMeHCI::Application.config.secret_token = '871e5e6ac80f5b0f0e66285f3613e636749d606f8f54bb7253f49ae1eeded2b4b44226a57b14558cfd36d1fc337794a3eda7811fd31b7db6d04e8f94203f2378'
